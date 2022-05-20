da5928074e4f7cd7301082253fe3982ef177ab5a
Erykzon <jota.once6mail.com>
2022-05-19 20:22:53 -0500

    Agregando tabla de mission commander

---
+++ src/services/MissionCommanderS.js
@@ -0,0 +1,22 @@
+import http from "../http-common"
+
+class MissioncommanderS{
+
+    getAll(){
+        return http.get("missionCommander");
+    }
+    get(id){
+        return http.get(`/missionCommander/${id}`);
+    }
+    create(data){
+        return http.post("/missionCommander", data);
+    }
+    update(id, data){
+        return http.put(`/missionCommander/${id}`, data);
+    }
+    delete(id){
+        return http.delete(`/missionCommander/${id}`);
+    }
+}
+
+export default new MissioncommanderS();
